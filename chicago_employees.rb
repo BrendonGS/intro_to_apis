47670ca5345854417d10ed46ea1e9d60fe97d2f1
BrendonGS <bgsggermany@gmail.com>
2019-01-11 10:47:53 -0600

    intro_to_apis

---
+++ chicago_employees.rb
@@ -0,0 +1,13 @@
+require "http"
+
+response = HTTP.get ("https://data.cityofchicago.org/resource/xzkq-xp2w.json")
+employees = response.parse
+
+employees.each do |employee|
+  name = employee["name"]
+  salary = employee["annual_salary"]
+  puts "#{name} makes $#{salary}"
+end
+
+p employees.count
+# p HTTP.get("http://github.com")to_s
